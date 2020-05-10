0c75ba13d564ef4bb368c74be830ff2d6bac89b8
Douglas Naphas <douglasnaphas@gmail.com>
2020-05-10 09:42:53 -0400

    Use a Select from react-select

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Select from "react-select";
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const handleChange = (s) => {
+    console.log(`setting...`, s, setSelectedOption(s));
+  };
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <br />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          onClick={() => {
-            console.log("another link clicked");
-          }}
-        >
-          another link
-        </a>
-        <br></br>
-        <a href="https://www.google.com" target="_blank">
-          <p>
-            This has<em>content</em>in it
-          </p>
-        </a>
-      </header>
+      <div>
+        <Select
+          value={selectedOption}
+          onChange={handleChange}
+          options={options}
+        ></Select>
+      </div>
     </div>
   );
 }
